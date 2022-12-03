3d7c66b9e0c38f91d23f2fa8fb620329ab930953
Chaitanya Kumar <chaitanyak28771@gmail.com>
2022-12-03 22:59:19 +0100

    Complete day 3 part 2

--- src/AoC2022.jl
+++ src/AoC2022.jl
@@ -88,7 +88,7 @@ function priority(c)
 end
 
 function duplicate_priorities(s)
-  pivot = length(s)÷2
+  pivot = length(s)Ã·2
   c1 = Set(s[1:pivot])
   c2 = Set(s[pivot+1:end])
   duplicates = intersect(c1, c2)
@@ -100,8 +100,19 @@ function solve3_1(input_str)
   sum(duplicate_priorities.(input_lines))
 end
 
+function find_duplicates(s)
+  intersect(s...)
+end
+
 function solve3_2(input_str)
-  # TODO
+  input_lines = split(input_str, '\n')
+  total_priority = 0
+  for i in 1:3:length(input_lines)
+    group = input_lines[i:i+2]
+    dups = find_duplicates(group)
+    total_priority += sum(priority.(dups))
+  end
+  total_priority
 end
 
 end  # module AoC2022
