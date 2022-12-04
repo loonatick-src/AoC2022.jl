d6197ec9fd67a8eeb8a0387b296761c506fa059e
Chaitanya Kumar <chaitanyak28771@gmail.com>
2022-12-04 11:30:52 +0100

    Day 4 complete

--- src/AoC2022.jl
+++ src/AoC2022.jl
@@ -1,6 +1,25 @@
 module AoC2022
 
 using Match: @match
+import Base: in
+
+function in(x::UnitRange{T}, y::UnitRange{T}) where {T}
+  x.start >= y.start && x.stop <= y.stop
+end
+
+function partial_overlap(x::R, y::R) where {R <: UnitRange}
+  x.start <= y.start && x.stop <= y.stop && x.stop >= y.start
+end
+
+function overlap(x::R, y::R) where {R <: UnitRange}
+  x ∈ y ||
+    y ∈ x ||
+    partial_overlap(x, y) ||
+    partial_overlap(y, x)
+      
+end
+
+const ∧ = overlap
 
 const DATA_DIR = joinpath("..", "..", "data")
 
@@ -113,6 +132,41 @@ function solve3_2(input_str)
     total_priority += sum(priority.(dups))
   end
   total_priority
+end
+
+
+#== DAY 4 ==#
+function solve4_1(input_str)
+  task_pairs = split(input_str, '\n')
+  split_tasks = split.(task_pairs, ',')
+  nredundant_tasksets = 0
+  for p in split_tasks
+    tis = parse_interval.(p)
+    if tis[1] ∈ tis[2] || tis[2] ∈ tis[1]
+      nredundant_tasksets +=1
+    end
+  end
+  nredundant_tasksets
+end
+
+"""Same as `sovle4_1`, but ∈ → ∧"""
+function solve4_2(input_str)
+  task_pairs = split(input_str, '\n')
+  split_tasks = split.(task_pairs, ',')
+  nredundant_tasksets = 0
+  for p in split_tasks
+    tis = parse_interval.(p)
+    if tis[1] ∧ tis[2]
+      @show tis
+      nredundant_tasksets +=1
+    end
+  end
+  nredundant_tasksets
+end
+
+function parse_interval(s)
+  interval_lims = parse.(Int, split(s, '-'))
+  interval_lims[1]:interval_lims[2]
 end
 
 end  # module AoC2022
