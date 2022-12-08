f58fed252e5028adcfd41056b3ac72cc10bd4a31
Chaitanya Kumar <chaitanyak28771@gmail.com>
2022-12-09 00:58:37 +0100

    Solved day 7  part 1 in a very non-idiomatic way

--- src/AoC2022.jl
+++ src/AoC2022.jl
@@ -29,7 +29,6 @@ split_newline(s) = split(s, '\n')
 
 const DATA_DIR = joinpath("..", "..", "data")
 
-
 function read_input(day::I) where {I <: Integer}
   input_path = joinpath(DATA_DIR,
                         string(day)) * ".txt"
@@ -161,7 +160,7 @@ function overlap(x::R, y::R) where {R <: UnitRange}
     y ⊂ x ||
     partial_overlap(x, y) ||
     partial_overlap(y, x)
-      
+  
 end
 
 const ∧ = overlap
@@ -328,7 +327,14 @@ end
 mutable struct DirNode
   children::Vector{Union{Int, DirNode}}
   parent::Union{DirNode, Nothing}
+  name::String
+  size::Int
+
+  DirNode(name) = new(Union{Int, DirNode}[], nothing, name, 0)
+  DirNode(parent, name) = new(Union{Int, DirNode}[], parent, name, 0)
 end
+
+FileOrDir = Union{Int, DirNode}
 
 parse_filesize(s) = s |> split_whitespace |> first |> parse_int
 
@@ -342,15 +348,15 @@ function parse_cmd(s)
   end
 end
 
-function process_ls!(ds, curr_dir, input_lines, line_number)
+function process_ls!(ds::DirNode, curr_dir::DirNode, input_lines::Vector{SubString{String}}, line_number::Integer)
   line_number += 1
   while line_number <= length(input_lines)
     line = input_lines[line_number]
     split_line = split_whitespace(line)
     if (split_line[1]) == "dir"
-      push!(curr_dir.children, DirNode(Union{Int, DirNode}[], curr_dir))
+      push!(curr_dir.children, DirNode(curr_dir, split_line[2]))
     elseif split_line[1] != raw"$"
-      push!(cur_dir.children, parse_filesize(line))
+      push!(curr_dir.children, parse_filesize(line))
     else
       return line_number
     end
@@ -360,12 +366,14 @@ function process_ls!(ds, curr_dir, input_lines, line_number)
 end
 
 function process_cd!(ds, curr_dir, line)
-  dir_name = split_whitespace(line)[2]
+  dir_name = split_whitespace(line)[3]
   if dir_name == ".."
     return curr_dir.parent
   else
-    push!(curr_dir.children, DirNode(Union{Int,DirNode}[], curr_dir))
-    return curr_dir.children[end]
+    for item in curr_dir.children
+      if item isa DirNode && item.name == dir_name && return item
+      end
+    end
   end
 end
 
@@ -373,8 +381,8 @@ function reduce!(ds)
   # TODO
 end
 
-function solve7_1(s)
-  ds = DirNode(Union{Int, DirNode}[], nothing)
+function construct_dirtree(s)
+  ds = DirNode("/")
   input_lines = split(s, '\n')
   curr_dir = ds
   line_number = 2
@@ -385,12 +393,33 @@ function solve7_1(s)
       line_number = process_ls!(ds, curr_dir, input_lines, line_number)
     else
       curr_dir = process_cd!(ds, curr_dir, l)
+      line_number += 1
     end
   end
   ds
 end
 
-function solve7_2(s)
+function calculate_dir_size(dir::DirNode, sub_size)
+  total_size = 0
+  for item in dir.children
+    if item isa Integer
+      total_size += item
+    else
+      # this can be made tail-recursive
+      (item.size, sub_size) = calculate_dir_size(item, sub_size)
+      if item.size <= 100_000
+        sub_size += item.size
+      end
+      total_size += item.size
+    end
+  end
+  (total_size, sub_size)
+end
+
+function solve7_1(s)
+  dir_tree = construct_dirtree(s)
+  (dir_tree.size, sub_size) = calculate_dir_size(dir_tree, 0)
+  sub_size
 end
 
 end  # module AoC2022
