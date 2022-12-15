bba8e0c16345395743ff37d477167e48af6383fc
Chaitanya Kumar <chaitanyak28771@gmail.com>
2022-12-15 01:11:43 +0100

    Might have completed day 09, misinput my answer haha

--- src/day09.jl
+++ src/day09.jl
@@ -20,27 +20,27 @@ clamp_unit_box(x) = clamp(x, -1:1)
 
 clamp_displacement(d) = clamp_unit_box.(d)
 
-function simulate_rope(head_pos, tail_pos, m)
-  head_pos = head_pos .+ m
+function simulate_rope(head_pos, tail_pos)
   Δd = head_pos .- tail_pos
   p = prod(Δd)
-  s = sum(Δd)
-  if Δd[1] == Δd[2] == 0
-    (head_pos, tail_pos)
-  elseif abs.(Δd) == @SVector [1, 1]
-    (head_pos, tail_pos)
-  elseif sum(abs.(Δd)) == 1
-    (head_pos, tail_pos)
-  elseif p == 0
-    # head and tail are in the same line
-    tail_pos = tail_pos .+ m
-    (head_pos, tail_pos)
-  elseif sum(abs.(Δd)) == 1
-    (head_pos, tail_pos)
+  s_abs = sum(abs.(Δd))
+  if p == 0
+    # they are along the same horizontal or vertical
+    if s_abs <= 1
+      # println("$(head_pos) and $(tail_pos) are touching or have complete overlap")
+      # complete overlap or touching
+    else
+      # println("$(head_pos) and $(tail_pos) are along the same axis but not touching")
+      tail_pos = tail_pos .+ clamp_displacement(Δd)
+    end
+  elseif s_abs <= 2
+    # they are touching diagonally
+    # println("$(head_pos) and $(tail_pos) are touching diagonally")
   else
+    # println("$(head_pos) and $(tail_pos) are not in the same axis, not touching")
     tail_pos = tail_pos .+ clamp_displacement(Δd)
-    (head_pos, tail_pos)
   end
+  tail_pos
 end
 
 
@@ -85,20 +85,21 @@ function solve9_1(s)
 end
 
 function simulate_longer_rope!(visited, knot_positions, m, d)
+  N = length(knot_positions)
   for k in 1:d
-    for (i, p) in enumerate(knot_positions[begin:end-1])
-      head_pos = p
-      tail_pos = knot_positions[i+1]
-      head_pos, tail_pos = simulate_rope(head_pos, tail_pos, m)
-      knot_positions[i] = head_pos
-      knot_positions[i+1] = tail_pos
+    # move the head
+    new_head = knot_positions[1] .+ m
+    knot_positions[1] = new_head
+    for i in 2:N
+      # propagate changes to trailing knots
+      h = knot_positions[i-1]
+      t = knot_positions[i]
+      new_tail = simulate_rope(h, t)
+      knot_positions[i] = new_tail
     end
+    # @show visualize_knots(knot_positions)
     push!(visited, knot_positions[end])
   end
-end
-
-function longer_rope_step!(visited, knot_positions, m, k, d)
-  
 end
 
 function solve9_2(s)
@@ -113,6 +114,7 @@ function solve9_2(s)
     simulate_longer_rope!(visited, knot_positions, m, d)
   end
   length(visited)
+  # visualize(visited)
 end
 
 function visualize(visited)
@@ -127,7 +129,6 @@ function visualize(visited)
     max_y = max(max_y, y)
   end
   dims = (max_x - min_x+1, max_y - min_y+1)
-  @show dims
   board = Matrix{Char}(undef, dims...)
   fill!(board, '.')
   for xy in visited
@@ -135,6 +136,29 @@ function visualize(visited)
     x = x - min_x + 1
     y = y - min_y + 1
     board[x,y] = '#'
+  end
+  board
+end
+
+function visualize_knots(knot_positions)
+  xs = getindex.(knot_positions, 1)
+  ys = getindex.(knot_positions, 2)
+  x_min = minimum(xs)
+  y_min = minimum(ys)
+  x_max = maximum(xs)
+  y_max = maximum(ys)
+  
+  ncols = x_max - x_min + 1
+  nrows = y_max - y_min + 1
+  board = Matrix{Char}(undef, nrows, ncols)
+  fill!(board, '.')
+  for (i,xy) in enumerate(knot_positions)
+    x, y = xy
+    x = x - x_min + 1
+    y = y - y_min + 1
+    if board[y,x] == '.'
+      board[y,x] = string(i)[1]
+    end
   end
   board
 end
